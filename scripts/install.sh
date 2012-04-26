9b93ee4aae56e4780847d388ded08dee46c4361e
Justin Too <doubleotoo@gmail.com>
2012-04-26 15:43:47 -0700

    Add peter-murach/github as a submodule in gems/github.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,11 @@
 gem build github_flow.gemspec
 gem install ./github_flow-0.0.0.gem
 
+pushd gems/github
+gem build ./github_api.gemspec
+gem install ./github-0.4.11.gem
+popd
+
 # pushd gems/git-pulls
 # gem build ./git-pulls.gemspec
 # gem install ./git-pulls-0.3.4.gem
